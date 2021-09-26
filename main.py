e17189c72fb5b3750d211df3697813e3f8923740
Mikhail <mkl-by@yandex.ru>
2021-09-26 22:27:18 +0300

    add tkinter function

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ import os
 import win32com.client as win32
 from win32com.client import constants
 from docx import Document
+import window
 #  дописать ввод на ткинтер, и удаление doc файлов, сделать exe!!!!!!!!!!!!!!!!!!!!!!!
 
+
 def search_str(
-        path_one: str,
-        search_str: str,
-        what_to_replace: str
+        path_one: str,  # путь к изменяемому файлу
+        search_str: str,  # строка которую необходимо найти
+        what_to_replace: str  # на что заменить строку
                 ) -> None:
     """
     функция получает путь к файлу docx и текст который необходимо изменить и
@@ -18,12 +20,12 @@ def search_str(
     document = Document(path_one)
     for paragraph in document.paragraphs:
         if search_str in paragraph.text:
-            print(paragraph.text)
+            print(paragraph.text, 'найдено')
             paragraph.text = re.sub(search_str, what_to_replace, paragraph.text).strip()
         document.save(path_one)
 
 
-def save_as_docx(path_one):
+def save_as_docx(path_one: str) -> None:
     # Opening MS Word
     word = win32.gencache.EnsureDispatch('Word.Application')
     doc = word.Documents.Open(path_one)
@@ -40,14 +42,14 @@ def save_as_docx(path_one):
 
 if __name__ == '__main__':
 
-    # конвертируем файлы doc в docx
-    paths = glob('C:\\Users\\mkl\\Desktop\\*.doc', recursive=True)
-    for path_one in paths:
-        save_as_docx(path_one)
+    """# конвертируем файлы doc в docx
+    # paths = glob('C:\\Users\\mkl\\Desktop\\*.doc', recursive=True)
+    # for path_one in paths:
+    #     save_as_docx(path_one)
 
     # изменяем информацию в файле docx
-    paths = glob('C:\\Users\\mkl\\Desktop\\*.docx', recursive=True)
-    for path_one in paths:
-        if os.path.split(path_one)[1].split('.')[1] == 'docx':
-            search_str(path_one, 'Лекунович М.К.', '****')
-
+    # paths = glob('C:\\Users\\mkl\\Desktop\\*.docx', recursive=True)
+    # for path_one in paths:
+    #     if os.path.split(path_one)[1].split('.')[1] == 'docx':
+    #         search_str(path_one, 'Лекунович М.К.', '****')
+"""
--- window.py
+++ window.py
@@ -1,23 +1,71 @@
 from tkinter import *
-from tkinter import ttk
+from tkinter import Tk, ttk, filedialog
+from glob import glob
+import main
+import os
+# root.withdraw()  # скрываем экземпляр
 
-root = Tk()
-root.title('Название')
 
-mainframe = ttk.Frame(root, padding="3 3 12 12")
-mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-root.columnconfigure(0, weight=1)
-root.rowconfigure(0, weight=1)
-feet = StringVar()
-feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet)
-feet_entry.grid(column=2, row=1, sticky=(W, E))
+def transform_in_docx(paths):
+    for path_one in paths:
+        result.set(path_one)
+        main.save_as_docx(path_one)
 
-meters = StringVar()
-ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
 
-ttk.Button(mainframe, text="Calculate").grid(column=3, row=3, sticky=W)
+def replace_txt(paths):
+    for path_one in paths:
+        if os.path.split(path_one)[1].split('.')[1] == 'docx':
+            result.set(path_one)
+            main.search_str(path_one, 'Лекунович М.К.', '****')
 
-ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
-ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
-ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
-root.mainloop()
+
+def open_directory():
+    pat = filedialog.askdirectory()  # выбираем папку для преобразования
+    paths_doc = glob(pat+'/*.doc', recursive=True)  # рекурсивно обходим папку с файлами *doc
+    transform_in_docx(paths_doc)
+    paths_doc = glob(pat + '/*.docx', recursive=True)
+    replace_txt(paths_doc)
+
+
+if __name__ == '__main__':
+
+    root = Tk()
+    root.title('Преобразовать, удалить, и заменить')
+    result = StringVar()
+    result.set('path')
+    but = ttk.Button(root, text='open dir and transform', command=open_directory).grid(row=0, column=0)
+
+    label = ttk.Label(root, text=result.get()).grid(row=1, column=0)
+
+
+
+# frame = ttk.Frame(rootpadding="3 3 12 12")
+# mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+# root.columnconfigure(0, weight=1)
+# root.rowconfigure(0, weight=1)
+# feet = StringVar()
+# feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet)
+# feet_entry.grid(column=2, row=1, sticky=(W, E))
+#
+# meters = StringVar()
+# ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
+#
+# ttk.Button(mainframe, text="Calculate").grid(column=3, row=3, sticky=W)
+#
+# ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
+# ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
+# ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
+    root.mainloop()
+# import os
+# import subprocess
+# FILEBROWSER_PATH = os.path.join(os.getenv('WINDIR'), 'explorer.exe')
+#
+# def explore(path):
+#     # explorer would choke on forward slashes
+#     path = os.path.normpath(path)
+#
+#     if os.path.isdir(path):
+#         subprocess.run([FILEBROWSER_PATH, path])
+#     elif os.path.isfile(path):
+#         subprocess.run([FILEBROWSER_PATH, '/select,', os.path.normpath(path)])
+
