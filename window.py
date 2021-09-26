381e10bea0b520650f71e2e30baf09f943dc7ce7
Mikhail <mkl-by@yandex.ru>
2021-09-26 22:27:55 +0300

    add tkinter function

--- window.py
+++ window.py
@@ -34,38 +34,5 @@ if __name__ == '__main__':
     result = StringVar()
     result.set('path')
     but = ttk.Button(root, text='open dir and transform', command=open_directory).grid(row=0, column=0)
-
     label = ttk.Label(root, text=result.get()).grid(row=1, column=0)
-
-
-
-# frame = ttk.Frame(rootpadding="3 3 12 12")
-# mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-# root.columnconfigure(0, weight=1)
-# root.rowconfigure(0, weight=1)
-# feet = StringVar()
-# feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet)
-# feet_entry.grid(column=2, row=1, sticky=(W, E))
-#
-# meters = StringVar()
-# ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
-#
-# ttk.Button(mainframe, text="Calculate").grid(column=3, row=3, sticky=W)
-#
-# ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
-# ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
-# ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
     root.mainloop()
-# import os
-# import subprocess
-# FILEBROWSER_PATH = os.path.join(os.getenv('WINDIR'), 'explorer.exe')
-#
-# def explore(path):
-#     # explorer would choke on forward slashes
-#     path = os.path.normpath(path)
-#
-#     if os.path.isdir(path):
-#         subprocess.run([FILEBROWSER_PATH, path])
-#     elif os.path.isfile(path):
-#         subprocess.run([FILEBROWSER_PATH, '/select,', os.path.normpath(path)])
-
